81f04280e60b29fa2feb71f1a02a1b225057b8b2
Volkofff666 <danil.volckoff@yandex.ru>
2025-12-01 10:22:39 +0500

    fix: remove webpack config, use turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   compress: true,
   poweredByHeader: false,
   
-  
+  // Это обязательно для Next.js 16
   turbopack: {},
   
- 
   experimental: {
-    
     optimizePackageImports: [
       '@vercel/analytics', 
       '@vercel/speed-insights'
     ],
-  
     webpackBuildWorker: true,
-    
     optimizeCss: true,
   },
   
- 
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   
-
   headers: async () => [
     {
       source: '/:path*',
@@ -38,21 +31,8 @@ const nextConfig: NextConfig = {
           value: 'on'
         },
         {
-          key: 'Strict-Transport-Security',
-          value: 'max-age=63072000; includeSubDomains; preload'
-        },
-        {
           key: 'X-Content-Type-Options',
           value: 'nosniff'
-        },
-      ],
-    },
-    {
-      source: '/fonts/:path*',
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=31536000, immutable',
         },
       ],
     },
