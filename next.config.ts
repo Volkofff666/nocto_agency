39feef454d747978136076d0325cb71ccb417001
Volkofff666 <danil.volckoff@yandex.ru>
2025-12-01 10:09:33 +0500

    Fix 0.4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compress: true,
+  poweredByHeader: false,
+  
+  experimental: {
+    optimizePackageImports: ['@vercel/analytics', '@vercel/speed-insights'],
+    webpackBuildWorker: true,
+  },
+  
+  // Уменьшить количество chunks
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        framework: {
+          name: 'framework',
+          chunks: 'all',
+          test: /[\\/]node_modules[\\/](react|react-dom|scheduler)[\\/]/,
+          priority: 40,
+          enforce: true,
+        },
+        lib: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'lib',
+          priority: 30,
+          minChunks: 1,
+          reuseExistingChunk: true,
+        },
+      },
+      maxInitialRequests: 5, // Уменьшить с дефолтных 25
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
